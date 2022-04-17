2c30ec081ea79841f09ce3cfd2c306247bc86ffa
Alan <94911542+AlanDD13@users.noreply.github.com>
2022-04-17 21:05:56 +0300

    change and authorization blueprints

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, redirect
-from flask_login import LoginManager, login_user, login_required, logout_user
-from form.user import RegisterForm
-from form.login import LoginForm
-from form.change_password import ChangePasswordForm
-from form.change_username import ChangeUsernameForm
-from form.change_email import ChangeEmailForm
+from flask import Flask, render_template
+from flask_login import login_required, LoginManager
 from data import db_session
+from change import change
 from data.users import User
+from authorization import authorization
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'laksjflaksjf'
+app.register_blueprint(change)
+app.register_blueprint(authorization)
 
 
 login_manager = LoginManager()
@@ -28,121 +27,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="Passwords don't match")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Registration',
-                                   form=form,
-                                   message="This User already exists")
-        user = User(
-            username=form.name.data,
-            email=form.email.data,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Registration', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Wrong email or password",
-                               form=form)
-    return render_template('login.html', title='Authorization', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect('/')
-
-
 @app.route('/account')
 @login_required
 def account():
     return render_template('account.html')
-
-
-@app.route('/account/change_password', methods=['GET', 'POST'])
-@login_required
-def change_password():
-    form = ChangePasswordForm()
-    if form.validate_on_submit():
-        if form.new_password.data == form.new_password_submit.data:
-            db_sess = db_session.create_session()
-            user = db_sess.query(User).filter(User.email == form.email.data).first()
-            if user and user.check_password(form.new_password.data):
-                return render_template('change_password.html',
-                                       message="Password shouldn't be same as old",
-                                       form=form)
-            else:
-                user.set_password(form.new_password.data)
-                db_sess.commit()
-                return redirect('/')
-        else:
-            return render_template('change_password.html',
-                                   message="Passwords are different",
-                                   form=form)
-    return render_template('change_password.html', form=form)
-
-
-@app.route('/account/change_username', methods=['GET', 'POST'])
-@login_required
-def change_username():
-    form = ChangeUsernameForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            user.set_new_username(form.name.data)
-            db_sess.commit()
-            return redirect('/')
-        else:
-            return render_template('change_username.html',
-                                   message="Password doesn't fit",
-                                   form=form)
-    return render_template('change_username.html',form=form)
-
-
-@app.route('/account/change_email', methods=['GET', 'POST'])
-@login_required
-def change_email():
-    form = ChangeEmailForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        exists = db_sess.query(User.id).filter(User.email == form.new_email.data).first() is not None
-        if exists:
-            return render_template('change_email.html',
-                                message="User with this email already exists",
-                                form=form)
-        else:
-            if user and user.check_password(form.password.data):
-                user.set_new_email(form.new_email.data)
-                db_sess.commit()
-                return redirect('/')
-            else:
-                return render_template('change_email.html',
-                                   message="Password doesn't fit",
-                                   form=form)
-    return render_template('change_email.html', form=form)
 
 
 @app.route('/about_Kripto')
